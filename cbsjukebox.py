de61ff20d3ab952f612e031e653358f323e9520e
Stuart Campbell <stuart@harto.org>
2013-11-06 19:38:54 +1100

    WIP

---
+++ cbsjukebox.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Scrapes the CBS Orchestra playlist from Anton Fig's website and downloads a
+# sample of each song.
+
+import bs4
+import re
+import urllib2
+
+
+def parse_playlist(url='http://www.antonfig.com/lnsongs.htm'):
+    """
+    Parses the playlist URL to produce a list of (artist, title) tuples."
+    """
+
+    html = urllib2.urlopen(url).read()
+    soup = bs4.BeautifulSoup(html)
+
+    # :-(
+    playlist_table = soup.find('table', border=1, cellpadding=4, width='92%', align='center')
+    # :-(
+    playlist_rows = filter(lambda tr: 'bgcolor' not in tr.attrs, playlist_table.find_all('tr')[1:])
+
+    cleanup_whitespace = lambda s: re.sub(r'\s+', ' ', s, 0, re.MULTILINE)
+    normalise_artist = lambda s: s#re.sub(r'(.+), (.+)', r'\2 \1', s).strip()
+    normalise_title = lambda s: re.sub(r'"([^"]+)".*', r'\1', s).strip()
+
+    def to_playlist_entry(row):
+        artist, title = map(cleanup_whitespace, row.stripped_strings)
+        return (normalise_artist(artist), normalise_title(title))
+
+    return map(to_playlist_entry, playlist_rows)
+
+
+def extract_audio(artist, title):
+    # TODO: see https://github.com/ralphbean/clip-the-gym/blob/master/clipthegym/__init__.py
+
+
+if __name__ == '__main__':
+    playlist = parse_playlist()
+    for artist, title in playlist:
+        print artist, title
